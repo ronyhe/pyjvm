7b80b24f9213494c198c37506d4e62c3291e1fb1
ronyhe <ronyhe@gmail.com>
2018-06-23 13:18:39 +0300

    Added icons_<n> instructions

--- pyjvm/execution/constant_instructions.py
+++ pyjvm/execution/constant_instructions.py
@@ -1,5 +1,5 @@
 from pyjvm.execution.execution import bytecode, Executor
-from pyjvm.types import NULL_VALUE
+from pyjvm.types import NULL_VALUE, Integer
 
 
 @bytecode('nop')
@@ -8,8 +8,13 @@ class NoOp(Executor):
         pass
 
 
-# noinspection SpellCheckingInspection
 @bytecode('aconst_null', NULL_VALUE)
+@bytecode('iconst_0', Integer.create_instance(0))
+@bytecode('iconst_1', Integer.create_instance(1))
+@bytecode('iconst_2', Integer.create_instance(2))
+@bytecode('iconst_3', Integer.create_instance(3))
+@bytecode('iconst_4', Integer.create_instance(4))
+@bytecode('iconst_5', Integer.create_instance(5))
 class Push(Executor):
     def __init__(self, instruction, machine, value):
         super().__init__(instruction, machine)
--- test/test_constant_instructions.py
+++ test/test_constant_instructions.py
@@ -1,4 +1,4 @@
-from pyjvm.types import NULL_VALUE
+from pyjvm.types import NULL_VALUE, Integer
 from test.test_utils import BlankTestMachine
 
 
@@ -6,3 +6,10 @@ def test_a_const_null():
     machine = BlankTestMachine()
     machine.step_instruction('aconst_null')
     assert machine.current_op_stack().peek() == NULL_VALUE
+
+
+def test_int_consts():
+    machine = BlankTestMachine()
+    for i in range(6):
+        machine.step_instruction('iconst_' + str(i))
+        assert machine.current_op_stack().peek() == Integer.create_instance(i)
