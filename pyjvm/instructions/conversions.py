055b659e25b62df5b972a928fe56d46277ac0902
ronyhe <ronyhe@gmail.com>
2018-06-26 07:38:22 +0300

    Added conversions instructions

--- pyjvm/instructions/conversions.py
+++ pyjvm/instructions/conversions.py
@@ -1,10 +1,32 @@
 from pyjvm.instructions.instructions import Executor, bytecode
 from pyjvm.instructions.verifiers import verifier_by_type
-from pyjvm.jvm_types import Long, Integer, Float
+from pyjvm.jvm_types import Long, Integer, Float, Double
+
+_letters = {
+    'l': Long,
+    'f': Float,
+    'd': Double,
+    'i': Integer,
+    'b': Integer,
+    'c': Integer,
+    's': Integer
+}
+
+_instructions = 'i2l, i2f, i2d, l2i, l2f, l2d, f2i, f2l, f2d, d2i, d2l, d2f, i2b, i2c, i2s'.split(', ')
 
 
-@bytecode('i2l', Integer, Long)
-@bytecode('i2f', Integer, Float)
+def _conversion_decorator(the_class):
+    for instruction in _instructions:
+        source_letter, _, target_letter = instruction
+        source_type = _letters[source_letter]
+        target_type = _letters[target_letter]
+        func = bytecode(instruction, source_type, target_type)
+        the_class = func(the_class)
+
+    return the_class
+
+
+@_conversion_decorator
 class Convert(Executor):
     def __init__(self, instruction, machine, source_type, target_type):
         super().__init__(instruction, machine)
