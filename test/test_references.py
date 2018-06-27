60f4a35af6526e3f3b9c27a6eefb9277e2edcfb4
ronyhe <ronyhe@gmail.com>
2018-06-27 21:46:14 +0300

    Refactor references tests

--- test/test_references.py
+++ test/test_references.py
@@ -8,50 +8,65 @@ from pyjvm.jvm_types import Integer, ObjectReferenceType
 from test.test_utils import BlankTestMachine
 
 
-def test_get_static():
-    class_name = 'ClassName'
-    field_name = 'fieldName'
-    descriptor = 'I'
+class _DummyClass:
+    DESCRIPTOR = 'I'
 
-    the_class = ClassFile.create(class_name)
-    field = the_class.fields.create(field_name, descriptor)
-    field.access_flags.set('acc_static', True)
-    machine = BlankTestMachine()
-    field_ref = machine.current_constants().create_field_ref(class_name, field_name, descriptor)
-    machine.class_loader = FixedClassLoader({class_name: convert_class_file(the_class)})
-    machine.load_class(class_name)
+    def __init__(self):
+        self.name = 'ClassName'
+        self.class_file = ClassFile.create(self.name)
+        self.instance_field = self.class_file.fields.create('instance_field', self.DESCRIPTOR)
+        self.class_field = self.class_file.fields.create('class_field', self.DESCRIPTOR)
+        self.class_field.access_flags.set('acc_static', True)
+        self.type = ObjectReferenceType(self.name)
+
+    def create_field_ref(self, constants, field):
+        return constants.create_field_ref(self.name, field.name, self.DESCRIPTOR)
+
+    def instance_field_ref(self, constants):
+        return self.create_field_ref(constants, self.instance_field)
+
+    def class_field_ref(self, constants):
+        return self.create_field_ref(constants, self.class_field)
+
+
+_DUMMY = _DummyClass()
+_LOADER = FixedClassLoader({_DUMMY.name: convert_class_file(_DUMMY.class_file)})
+
+
+def _machine():
+    m = BlankTestMachine()
+    m.class_loader = _LOADER
+    return m
+
+
+def test_get_static():
+    machine = _machine()
+    field_ref = _DUMMY.class_field_ref(machine.current_constants())
+    machine.load_class(_DUMMY.name)
     value = Integer.create_instance(4)
-    machine.statics[class_name][field_name] = value
+
+    machine.statics[_DUMMY.name][_DUMMY.class_field.name] = value
     machine.step_instruction('getstatic', [Operand(OperandTypes.CONSTANT_INDEX, field_ref.index)])
     assert machine.current_op_stack().peek() == value
 
 
 def test_new():
-    class_name = 'ClassName'
-    the_class = ClassFile.create(class_name)
-    machine = BlankTestMachine()
-    class_ref = machine.current_constants().create_class(class_name)
-    machine.class_loader = FixedClassLoader({class_name: convert_class_file(the_class)})
+    machine = _machine()
+    class_ref = machine.current_constants().create_class(_DUMMY.name)
     machine.step_instruction('new', [Operand(OperandTypes.CONSTANT_INDEX, class_ref.index)])
-    assert machine.current_op_stack().peek() == ObjectReferenceType(class_name).create_instance(JvmObject(dict()))
+    tos = machine.current_op_stack().peek()
+    assert tos.type == _DUMMY.type
+    assert isinstance(tos.value, JvmObject)
 
 
 def test_new_inits_fields_to_defaults():
-    class_name = 'ClassName'
-    field_name = 'fieldName'
-    the_class = ClassFile.create(class_name)
-    the_class.fields.create(field_name, 'I')
-
-    machine = BlankTestMachine()
-    class_ref = machine.current_constants().create_class(class_name)
-    machine.class_loader = FixedClassLoader({class_name: convert_class_file(the_class)})
+    machine = _machine()
+    class_ref = machine.current_constants().create_class(_DUMMY.name)
     machine.step_instruction('new', [Operand(OperandTypes.CONSTANT_INDEX, class_ref.index)])
 
-    jvm_object = JvmObject({field_name: Integer.create_instance(0)})
-    expected = ObjectReferenceType(class_name).create_instance(jvm_object)
-
     actual = machine.current_op_stack().peek()
-    assert actual == expected
+    assert actual.type == _DUMMY.type
+    assert actual.value.fields[_DUMMY.instance_field.name.value] == Integer.create_instance(Integer.default_value)
 
 
 def test_new_inits_super_field_to_defaults():
