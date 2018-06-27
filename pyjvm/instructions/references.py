4063fdf5765a7329f3903fb8d5ece73fc9b2caea
ronyhe <ronyhe@gmail.com>
2018-06-28 02:37:31 +0300

    References refactor

--- pyjvm/instructions/references.py
+++ pyjvm/instructions/references.py
@@ -1,11 +1,17 @@
 from pyjvm.instructions.instructions import Executor, bytecode
 
 
+# noinspection PyAbstractClass
+class _ReferenceExecutor(Executor):
+    def constant_from_index(self):
+        index = int(self.instruction.operands[0].value)
+        return self.machine.current_constants()[index]
+
+
 @bytecode('putstatic')
-class PutStatic(Executor):
+class PutStatic(_ReferenceExecutor):
     def execute(self):
-        field_ref_index = int(self.instruction.operands[0].value)
-        field_ref = self.machine.current_constants()[field_ref_index]
+        field_ref = self.constant_from_index()
         class_name = field_ref.class_.name.value
         field_name = field_ref.name_and_type.name.value
         value = self.machine.current_op_stack().pop()
@@ -13,10 +19,9 @@ class PutStatic(Executor):
 
 
 @bytecode('getstatic')
-class GetStatic(Executor):
+class GetStatic(_ReferenceExecutor):
     def execute(self):
-        field_ref_index = int(self.instruction.operands[0].value)
-        field_ref = self.machine.current_constants()[field_ref_index]
+        field_ref = self.constant_from_index()
         class_name = field_ref.class_.name.value
         field_name = field_ref.name_and_type.name.value
         value = self.machine.get_static_field(class_name, field_name)
@@ -24,10 +29,9 @@ class GetStatic(Executor):
 
 
 @bytecode('new')
-class New(Executor):
+class New(_ReferenceExecutor):
     def execute(self):
-        class_ref_index = int(self.instruction.operands[0].value)
-        class_ref = self.machine.current_constants()[class_ref_index]
+        class_ref = self.constant_from_index()
         class_name = class_ref.name.value
         value = self.machine.create_new_class_instance(class_name)
         self.machine.current_op_stack().push(value)
