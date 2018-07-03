2b60d058361a3d5dc0646c5f3ab4a58707a6e7b6
ronyhe <ronyhe@gmail.com>
2018-07-03 19:15:47 +0300

    Refactored hierarchies

--- pyjvm/hierarchies.py
+++ pyjvm/hierarchies.py
@@ -17,25 +17,24 @@ def is_type_instance_of(instance_type: Type, descriptor_for_possible_parent: str
         raise ValueError('Cannot instance_of check value types. Only references')
 
     descriptor_type = convert_type(field_descriptor(descriptor_for_possible_parent))
-    return _is_type_instance_of_type(instance_type, descriptor_type, loader)
+    return _Checker(loader).is_instance_of(instance_type, descriptor_type)
 
 
-def _is_type_instance_of_type(instance_type, type_not_descriptor, loader):
-    descriptor_type = type_not_descriptor
-    if instance_type.is_array_reference and descriptor_type.is_array_reference:
-        return _is_array_instance_of_array(instance_type, descriptor_type, loader)
-    elif instance_type.is_class_reference and descriptor_type.is_class_reference:
-        return _is_class_instance_of_class(instance_type, descriptor_type, loader)
-    else:
-        return False
+class _Checker:
+    def __init__(self, loader):
+        self.loader = loader
 
+    def is_instance_of(self, instance_type, possible_parent_type):
+        if instance_type.is_array_reference and possible_parent_type.is_array_reference:
+            return self.is_instance_of(instance_type.refers_to, possible_parent_type.refers_to)
+        elif instance_type.is_class_reference and possible_parent_type.is_class_reference:
+            return self._is_class_instance_of_class(instance_type, possible_parent_type)
+        else:
+            return False
 
-def _is_array_instance_of_array(instance_type, descriptor_type, loader):
-    return _is_type_instance_of_type(instance_type.refers_to, descriptor_type.refers_to, loader)
-
-
-def _is_class_instance_of_class(instance_type, descriptor_type, loader):
-    instance_class_name = instance_type.refers_to
-    instance_interfaces = loader.get_the_class(instance_class_name).interfaces
-    possible_roots = list(instance_interfaces) + [instance_class_name]
-    return any(descriptor_type.refers_to in loader.get_ancestors(root) for root in possible_roots)
+    def _is_class_instance_of_class(self, instance_type, possible_parent_type):
+        instance_class_name = instance_type.refers_to
+        instance_class = self.loader.get_the_class(instance_class_name)
+        instance_interfaces = instance_class.interfaces
+        possible_roots = list(instance_interfaces) + [instance_class_name]
+        return any(possible_parent_type.refers_to in self.loader.get_ancestors(root) for root in possible_roots)
