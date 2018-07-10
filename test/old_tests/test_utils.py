a0a8d0ff6a740ccb9b25c2afa8a873ab5c3e5e3a
ronyhe <ronyhe@gmail.com>
2018-07-10 16:38:33 +0300

    Belongs to previous commit

--- test/old_tests/test_utils.py
+++ test/old_tests/test_utils.py
@@ -1,12 +1,9 @@
-from jawa.cf import ClassFile
 from jawa.constants import ConstantPool
 from jawa.util.bytecode import Instruction
 
-from pyjvm.class_loaders import FixedClassLoader, EmptyClassLoader
+from pyjvm.class_loaders import EmptyClassLoader
 from pyjvm.frame_locals import Locals
-from pyjvm.jawa_conversions import convert_class_file
 from pyjvm.jvm_class import JvmClass
-from pyjvm.jvm_types import ObjectReferenceType
 from pyjvm.machine import Frame, Machine
 from pyjvm.stack import Stack
 
@@ -30,33 +27,3 @@ class BlankTestMachine(Machine):
         self.run_frame(self.current_frame)
 
 
-class _DummyClass:
-    DESCRIPTOR = 'I'
-
-    def __init__(self):
-        self.name = 'ClassName'
-        self.class_file = ClassFile.create(self.name)
-        self.instance_field = self.class_file.fields.create('instance_field', self.DESCRIPTOR)
-        self.class_field = self.class_file.fields.create('class_field', self.DESCRIPTOR)
-        self.class_field.access_flags.set('acc_static', True)
-        self.type = ObjectReferenceType(self.name)
-
-    def create_field_ref(self, constants, field):
-        return constants.create_field_ref(self.name, field.name, self.DESCRIPTOR)
-
-    def instance_field_ref(self, constants):
-        return self.create_field_ref(constants, self.instance_field)
-
-    def class_field_ref(self, constants):
-        return self.create_field_ref(constants, self.class_field)
-
-
-DUMMY_SUB_CLASS_NAME = 'Sub'
-DUMMY_CLASS = _DummyClass()
-
-
-def dummy_loader():
-    return FixedClassLoader({
-        DUMMY_CLASS.name: convert_class_file(DUMMY_CLASS.class_file),
-        DUMMY_SUB_CLASS_NAME: convert_class_file(ClassFile.create(DUMMY_SUB_CLASS_NAME, DUMMY_CLASS.name))
-    })
--- test/utils.py
+++ test/utils.py
@@ -1,10 +1,13 @@
+from jawa.cf import ClassFile
 from jawa.constants import ConstantPool
 from jawa.util.bytecode import Instruction
 
 from pyjvm.actions import IncrementProgramCounter
+from pyjvm.class_loaders import FixedClassLoader
 from pyjvm.frame_locals import Locals
 from pyjvm.instructions.instructions import InstructorInputs, execute_instruction
-from pyjvm.jvm_types import Integer
+from pyjvm.jawa_conversions import convert_class_file
+from pyjvm.jvm_types import Integer, ObjectReferenceType
 from pyjvm.stack import Stack
 
 SOME_INT = Integer.create_instance(2)
@@ -59,3 +62,35 @@ def assert_incrementing_instruction(expected=None, **kwargs):
         expected = []
     expected.append(IncrementProgramCounter)
     assert_instruction(expected=expected, **kwargs)
+
+
+class _DummyClass:
+    DESCRIPTOR = 'I'
+
+    def __init__(self):
+        self.name = 'ClassName'
+        self.class_file = ClassFile.create(self.name)
+        self.instance_field = self.class_file.fields.create('instance_field', self.DESCRIPTOR)
+        self.class_field = self.class_file.fields.create('class_field', self.DESCRIPTOR)
+        self.class_field.access_flags.set('acc_static', True)
+        self.type = ObjectReferenceType(self.name)
+
+    def create_field_ref(self, constants, field):
+        return constants.create_field_ref(self.name, field.name, self.DESCRIPTOR)
+
+    def instance_field_ref(self, constants):
+        return self.create_field_ref(constants, self.instance_field)
+
+    def class_field_ref(self, constants):
+        return self.create_field_ref(constants, self.class_field)
+
+
+DUMMY_SUB_CLASS_NAME = 'Sub'
+DUMMY_CLASS = _DummyClass()
+
+
+def dummy_loader():
+    return FixedClassLoader({
+        DUMMY_CLASS.name: convert_class_file(DUMMY_CLASS.class_file),
+        DUMMY_SUB_CLASS_NAME: convert_class_file(ClassFile.create(DUMMY_SUB_CLASS_NAME, DUMMY_CLASS.name))
+    })
