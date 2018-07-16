d2ee1d2f696429141205606229e51d60a4971599
ronyhe <ronyhe@gmail.com>
2018-07-16 10:50:27 +0300

    Added invocation instructions

--- pyjvm/instructions/invocations.py
+++ pyjvm/instructions/invocations.py
@@ -3,15 +3,22 @@ from pyjvm.actions import IncrementProgramCounter
 from pyjvm.instructions.instructions import bytecode, Instructor
 
 
-@bytecode('invokevirtual')
+@bytecode('invokevirtual', 1)
+@bytecode('invokespecial', 1)
+@bytecode('invokeinterface', 1)
+@bytecode('invokestatic', 0)
 class InvokeVirtual(Instructor):
+    def __init__(self, inputs, args_to_add):
+        super().__init__(inputs)
+        self.args_to_add = args_to_add
+
     def execute(self):
         method_ref = self.operand_as_constant()
         class_name = method_ref.class_.name.value
         method_name = method_ref.name_and_type.name.value
         class_ = self.loader.get_the_class(class_name)
         method = class_.methods[method_name]
-        num_args = len(method.args) + 1  # Implicit `this` parameter
+        num_args = len(method.args) + self.args_to_add
         args = self.peek_many(num_args)
 
         return IncrementProgramCounter.after(
