217e6f81ec9f5bca59281a957b65be22af8fd023
ronyhe <ronyhe@gmail.com>
2018-07-19 15:06:11 +0300

    Added binary branch instructions

--- pyjvm/instructions/comparisons.py
+++ pyjvm/instructions/comparisons.py
@@ -3,9 +3,14 @@ import operator
 from pyjvm import actions
 from pyjvm.actions import IncrementProgramCounter, Actions
 from pyjvm.instructions.instructions import bytecode_dict, Instructor
-# noinspection SpellCheckingInspection
 from pyjvm.utils import bool_to_num
 
+
+def _dict_to_instruction_dict(d):
+    return {k: [v] for k, v in d.items()}
+
+
+# noinspection SpellCheckingInspection
 BOOLEAN_COMPARISONS = {
     'lcmp': operator.eq,
     'fcmpl': operator.le,
@@ -13,7 +18,6 @@ BOOLEAN_COMPARISONS = {
     'dcmpl': operator.le,
     'dcmpg': operator.ge,
 }
-_BOOLEAN_INSTRUCTION_DICT = {k: [v] for k, v in BOOLEAN_COMPARISONS.items()}
 
 # noinspection SpellCheckingInspection
 UNARY_BRANCH_COMPARISONS = {
@@ -24,10 +28,21 @@ UNARY_BRANCH_COMPARISONS = {
     'ifgt': operator.gt,
     'ifle': operator.le
 }
-_UNARY_BRANCH_COMPARISON_DICT = {k: [v] for k, v in UNARY_BRANCH_COMPARISONS.items()}
+
+# noinspection SpellCheckingInspection
+BINARY_BRANCH_COMPARISONS = {
+    "if_icmpeq": operator.eq,
+    "if_icmpne": operator.ne,
+    "if_icmplt": operator.lt,
+    "if_icmpge": operator.ge,
+    "if_icmpgt": operator.gt,
+    "if_icmple": operator.le,
+    "if_acmpeq": operator.eq,
+    "if_acmpne": operator.ne
+}
 
 
-@bytecode_dict(_BOOLEAN_INSTRUCTION_DICT)
+@bytecode_dict(_dict_to_instruction_dict(BOOLEAN_COMPARISONS))
 class BooleanComparison(Instructor):
     def __init__(self, inputs, op):
         super().__init__(inputs)
@@ -43,7 +58,7 @@ class BooleanComparison(Instructor):
         )
 
 
-@bytecode_dict(_UNARY_BRANCH_COMPARISON_DICT)
+@bytecode_dict(_dict_to_instruction_dict(UNARY_BRANCH_COMPARISONS))
 class UnaryBranchComparison(Instructor):
     def __init__(self, inputs, op):
         super().__init__(inputs)
@@ -63,5 +78,28 @@ class UnaryBranchComparison(Instructor):
 
         return Actions(
             actions.Pop(),
+            actions.GoTo(target)
+        )
+
+@bytecode_dict(_dict_to_instruction_dict(BINARY_BRANCH_COMPARISONS))
+class BinaryBranchComparison(Instructor):
+    def __init__(self, inputs, op):
+        super().__init__(inputs)
+        self.op = op
+
+    def execute(self):
+        values = self.peek_many(2)
+
+        result = self.op(*(value.value for value in values))
+        if result:
+            offset = self.operand_as_int()
+        else:
+            offset = 1
+
+        source = self.instruction.pos
+        target = source + offset
+
+        return Actions(
+            actions.Pop(2),
             actions.GoTo(target)
         )
--- test/test_comparisons.py
+++ test/test_comparisons.py
@@ -1,5 +1,5 @@
 from pyjvm.actions import Pop, Push, GoTo
-from pyjvm.instructions.comparisons import BOOLEAN_COMPARISONS, UNARY_BRANCH_COMPARISONS
+from pyjvm.instructions.comparisons import BOOLEAN_COMPARISONS, UNARY_BRANCH_COMPARISONS, BINARY_BRANCH_COMPARISONS
 from pyjvm.jvm_types import Integer
 from pyjvm.utils import bool_to_num
 from test.utils import assert_incrementing_instruction, assert_instruction, literal_instruction
@@ -37,4 +37,28 @@ def test_unary_branch_comparisons():
             instruction=instruction,
             op_stack=[Integer.create_instance(value)],
             expected=[Pop(), GoTo(target)]
+        )
+
+
+def test_binary_branch_comparisons():
+    offset = 6
+    source = 3
+
+    values = 4, 5
+    for name, op in BINARY_BRANCH_COMPARISONS.items():
+        # noinspection PyProtectedMember
+        instruction = literal_instruction(name, offset)._replace(pos=source)
+        result = op(*values)
+        if result:
+            target = source + offset
+        else:
+            target = source + 1
+
+        assert_instruction(
+            instruction=instruction,
+            op_stack = [Integer.create_instance(v) for v in values],
+            expected=[
+                Pop(2),
+                GoTo(target)
+            ]
         )
