cad933e0b76bad4947f4fef24f0df3e2dd2a2050
ronyhe <ronyhe@gmail.com>
2018-07-26 19:28:06 +0300

    Documented invocations.py

--- pyjvm/instructions/invocations.py
+++ pyjvm/instructions/invocations.py
@@ -9,6 +9,17 @@ from pyjvm.utils.jawa_conversions import key_from_method_ref
 @bytecode('invokeinterface', 1)
 @bytecode('invokestatic', 0)
 class InvokeVirtual(Instructor):
+    """Invokes methods
+
+    This Instructor uses the method's details to figure out how many parameters should be popped of the stack.
+    It also provides an `args_to_add` method to enable popping off the implicit `this` parameter for instance methods.
+
+    So the general process is:
+     - Get a reference to the method via the details in the instruction
+     - Extract number of parameters and add `args_to_add` to it
+     - Pop parameters and invoke with them
+    """
+
     def __init__(self, inputs, args_to_add):
         super().__init__(inputs)
         self.args_to_add = args_to_add
