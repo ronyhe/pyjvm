b08b5a40b1c9ab20a25a1f5cc3e1b9710edb4b67
ronyhe <ronyhe@gmail.com>
2018-07-27 00:07:51 +0300

    Documented frame_locals.py

--- pyjvm/model/frame_locals.py
+++ pyjvm/model/frame_locals.py
@@ -1,4 +1,13 @@
 class Locals:
+    """A local variable array for Frame objects
+
+    The main reason for a class, rather than a simple list, is that certain JVM types are expected
+    to hold two slots in this array.
+    This class aims to preserves that guarantee by using an internal `MISSING` object to mark inaccessible slots.
+    It also prevents uninitialized access at runtime.
+    However, a class that was emitted by a compliant Java compiler should already prevent such code from existing.
+    """
+
     MISSING = object()
 
     def __init__(self, length):
@@ -8,6 +17,7 @@ class Locals:
         self._locals: list = [None] * length
 
     def store(self, index, value):
+        """Store `value` at `index`"""
         if value is None:
             raise ValueError('Cannot store None in locals')
         self._locals[index] = value
@@ -15,6 +25,7 @@ class Locals:
             self._locals[index + 1] = self.MISSING
 
     def load(self, index):
+        """Load the value at `index`"""
         result = self._locals[index]
         if result is None:
             raise ValueError(f'Access to uninitialized local variable at index {index}')
