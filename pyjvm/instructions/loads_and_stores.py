0960afa4907ef75c347d445688cea8850584a9e5
ronyhe <ronyhe@gmail.com>
2018-07-26 19:42:05 +0300

    Documented loads_and_stores.py

--- pyjvm/instructions/loads_and_stores.py
+++ pyjvm/instructions/loads_and_stores.py
@@ -4,6 +4,17 @@ from pyjvm.instructions.instructions import Instructor, bytecode_dict, bytecode_
 
 
 def _create_store_load_dict(phrase):
+    """Create an instruction dictionary of the load or store instructions
+
+    For example, with the phrase 'load'
+    {
+        'iload': [None],
+        'iload_0': [0],
+        ...
+        'aload_3: [3]
+    }
+    """
+
     # noinspection SpellCheckingInspection
     type_letters = 'ilfda'
     indexes_in_locals = [None, 0, 1, 2, 3]
@@ -25,6 +36,17 @@ def _create_store_load_dict(phrase):
 
 
 def _create_into_array_list(phrase):
+    """Create load or store into array instructions
+
+    For example, with the phrase 'astore':
+    [
+        'iastore',
+        'fastore',
+        ...
+        'aastore'
+    ]
+    """
+
     # noinspection SpellCheckingInspection
     return [letter + phrase for letter in 'lfdibcsa']
 
@@ -37,6 +59,11 @@ _LOAD_FROM_ARRAY = bytecode_list(_create_into_array_list('aload'))
 
 @_STORE
 class StoreIntoLocals(Instructor):
+    """Pops stack and stores the value in to locals at `index_in_locals`
+
+    If `index_in_locals` is None, the index is taken from the instruction operand.
+    """
+
     def __init__(self, inputs, index_in_locals=None):
         super().__init__(inputs)
         if index_in_locals is None:
@@ -53,6 +80,8 @@ class StoreIntoLocals(Instructor):
 
 @_STORE_INTO_ARRAY
 class StoreIntoArray(Instructor):
+    """Pops `value`, `index` and `array` from stack and stores `value` into `array` at `index`"""
+
     def execute(self):
         value, index, array = (self.peek_op_stack(i) for i in range(3))
         if array.is_null:
@@ -66,6 +95,11 @@ class StoreIntoArray(Instructor):
 
 @_LOAD
 class LoadFromLocals(Instructor):
+    """Loads value from locals at `index_in_locals` and pushes it onto array
+
+    If `index_in_locals` is None, the index is taken from the instruction operand.
+    """
+
     def __init__(self, inputs, index_in_locals=None):
         super().__init__(inputs)
         if index_in_locals is not None:
@@ -82,6 +116,8 @@ class LoadFromLocals(Instructor):
 
 @_LOAD_FROM_ARRAY
 class LoadFromArray(Instructor):
+    """Pops an `array` and an `index` from the stack and pushed the value stores in `array` at `index`"""
+
     def execute(self):
         index = self.peek_op_stack()
         array = self.peek_op_stack(1)
