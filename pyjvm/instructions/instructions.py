590fff5715308bdbf6832646cd1a44965d2ccccc
ronyhe <ronyhe@gmail.com>
2018-07-26 15:38:33 +0300

    Started documenting instructions.py

--- pyjvm/instructions/instructions.py
+++ pyjvm/instructions/instructions.py
@@ -1,3 +1,21 @@
+"""
+This module implements a dispatch mechanism to execute JVM instruction.
+It also defines several tools that are used across instructions.
+
+At a high level, it provides a registry for `Instructor` instance, and several utility functions for subscribing to it.
+When `execute_instruction` is called, it dispatches it to the correct Instructor via the registry and returns the
+resulting `Actions`
+
+`bytecode`, `bytecode_dict` and `bytecode_list` are meant to be used as decorators for sub classes of `Instructor`.
+    .. code::
+
+        @bytecode('iload')
+        class ILoadImplementation(Instructor):
+            pass
+
+They leverage the `decorator` method of `ClassRegistry`, so refer to that class for more information.
+
+"""
 from collections import OrderedDict
 
 from pyjvm.actions import IncrementProgramCounter, Action, Actions
@@ -9,6 +27,27 @@ bytecode = _registry.decorator
 
 
 def bytecode_dict(names_and_args):
+    """A decorator that enables defining instructions and their corresponding arguments
+
+    For example, this code:
+        .. code::
+
+            @bytecode_dict({
+                'iload_0': [0],
+                'iload_1': [1]
+            })
+            class SomeInstructor(Instructor):
+                pass
+
+    Is equivalent to this code:
+        .. code::
+
+            @bytecode('iload_0', 0)
+            @bytecode('iload_1', 1)
+            class SomeInstructor(Instructor):
+                pass
+
+    """
     dic = OrderedDict(names_and_args)
 
     def func(class_):
@@ -21,10 +60,32 @@ def bytecode_dict(names_and_args):
 
 
 def bytecode_list(names):
+    """A decorator that enables defining several instructions for the same Instructor
+
+    For example, this code:
+        .. code::
+
+            @bytecode_list(['sipush', 'bipush'])
+            class SomeInstructor(Instructor):
+                pass
+
+    Is equivalent to this code:
+        .. code::
+
+            @bytecode('sipush')
+            @bytecode('bipush')
+            class SomeInstructor(Instructor):
+                pass
+    """
     return bytecode_dict({name: [] for name in names})
 
 
 class InstructorInputs:
+    """The inputs that a basic Instructor expects.
+
+    Sub classes can recieve other arguments using the usual method mechanisms
+    """
+
     # noinspection PyShadowingBuiltins
     def __init__(self, *, instruction, locals, op_stack, constants, loader):
         self.instruction = instruction
