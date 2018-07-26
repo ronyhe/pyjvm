8c83ba1c94ee30de67a4d050333ce5d34bc4efdc
ronyhe <ronyhe@gmail.com>
2018-07-26 13:33:00 +0300

    Documented module main.py

--- pyjvm/main.py
+++ pyjvm/main.py
@@ -36,6 +36,19 @@ def cli():
 
 @click.command()
 def instruction_report():
+    """A command that displays information regarding the instruction set
+
+        - How many commands are expected (exist in the spec)?
+        - How many of them are implemented?
+        - How many are missing, and what are their names?
+        - Do we have inadvertent instructions that don't exist in the spec?
+
+        .. code::
+
+            pyjvm instruction_report
+            205 implemented out of 205. Missing: 0
+            0 invented instructions
+    """
     present_instructions = set(get_implemented_instructions())
     expected_instructions = set(op['mnemonic'] for op in opcode_table.values())
     missing = expected_instructions.difference(present_instructions)
@@ -51,6 +64,7 @@ def instruction_report():
 
 @click.command()
 def action_report():
+    """A command that displays the :class:`Action` types that exist in the system"""
     names = Action.action_names()
     click.echo(f'{len(names)} commands:')
     for name in names:
@@ -60,6 +74,10 @@ def action_report():
 @click.command()
 @click.argument('path')
 def dump_class(path):
+    """A command that displays useful information that exists in a class file
+
+    A compliment to the JDK `javap` program.
+    """
     path = Path(path)
     with path.open(mode='rb') as f:
         cf = ClassFile(f)
@@ -70,6 +88,10 @@ def dump_class(path):
 @click.argument('path')
 @click.argument('name')
 def dump_class_from_jar(path, name):
+    """A command that displays useful information that exists in a class that is inside a JAR
+
+    Similar to `dump_class`
+    """
     loader = ClassLoader(path)
     utils.dump_class(loader[name], click.echo)
 
@@ -79,6 +101,12 @@ def dump_class_from_jar(path, name):
 @click.option('-cp', default='')
 @click.option('--report', is_flag=True)
 def run(main_class, cp, report):
+    """A command that executes a `main` method in a JVM class
+
+    :param main_class: The name of the class
+    :param cp: Colon separated class anf jar files
+    :param report: A bool representing whether or not to dump execution info to stdout
+    """
     loader = TraditionalLoader(cp)
     if report:
         echo = print
@@ -87,6 +115,7 @@ def run(main_class, cp, report):
     machine.run(loader, main_class, echo=echo)
 
 
+# Add the sub commands to the main command, as per the `click` documentation
 cli.add_command(run)
 cli.add_command(action_report)
 cli.add_command(instruction_report)
