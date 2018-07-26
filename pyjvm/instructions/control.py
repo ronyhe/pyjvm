f5d311a63435f6475a0773c2261cccbf91cd1f35
ronyhe <ronyhe@gmail.com>
2018-07-26 19:16:20 +0300

    Documented control.py

--- pyjvm/instructions/control.py
+++ pyjvm/instructions/control.py
@@ -5,14 +5,15 @@ from pyjvm.instructions.instructions import Instructor, bytecode_list, bytecode,
 from pyjvm.instructions.switches import LOOKUP_SWITCH, TABLE_SWITCH, TableSwitch, LookupSwitch
 
 # noinspection SpellCheckingInspection
-_RETURN_LETTERS = 'ilfda'
 RETURN_RESULT_INSTRUCTIONS = [
-    letter + 'return' for letter in _RETURN_LETTERS
+    letter + 'return' for letter in 'ilfda'
 ]
 
 
 @bytecode_list(RETURN_RESULT_INSTRUCTIONS)
 class ReturnResult(Instructor):
+    """Returns from a method with the value at top-of-stack"""
+
     def execute(self):
         result = self.peek_op_stack()
         return actions.ReturnResult(result)
@@ -20,6 +21,8 @@ class ReturnResult(Instructor):
 
 @bytecode('return')
 class ReturnVoid(Instructor):
+    """Returns from a method without a value"""
+
     def execute(self):
         return actions.ReturnVoid()
 
@@ -29,6 +32,15 @@ class ReturnVoid(Instructor):
     'goto_w'
 ])
 class GoTo(Instructor):
+    """Jump instructions
+
+    The instructions provide offsets, so this Instructor adds the current program counter,
+    creating an absolute value.
+
+    There is no need to handle the goto_w instruction specially.
+    The good people of the jawa library handle this during instruction parsing. Go jawa!
+    """
+
     def execute(self):
         source = self.instruction.pos
         offset = self.operand_as_int()
@@ -41,6 +53,12 @@ class GoTo(Instructor):
     LOOKUP_SWITCH: [LookupSwitch]
 })
 class Switch(Instructor):
+    """An instructor for complex jump instructions
+
+    The heavy lifting occurs via the `TableSwitch` and `LookupSwitch` classes.
+    See switches.py for more information.
+    """
+
     def __init__(self, inputs, switch_class):
         super().__init__(inputs)
         self.switch_class = switch_class
