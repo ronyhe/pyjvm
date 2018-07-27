d06002c1a323b7a026956ee09be375a8b096acce
ronyhe <ronyhe@gmail.com>
2018-07-28 02:00:24 +0300

    Documented utils.py

--- pyjvm/utils/utils.py
+++ pyjvm/utils/utils.py
@@ -1,9 +1,16 @@
+"""Miscellaneous useful functions"""
 from jawa.util.bytecode import Operand, OperandTypes
 
 from pyjvm.model.jvm_types import Integer
 
 
 def dump_class(cf, echo):
+    """Display information about a class file
+
+    Complements the functionality of the JDK javap tool.
+    :param cf: A ClassFile
+    :param echo: A print-like method
+    """
     echo(f'{cf.this.name.value} : {cf.super_.name.value}')
     for field in cf.fields:
         echo(f'\t{field.name.value}: {field.type}')
@@ -20,6 +27,15 @@ def dump_class(cf, echo):
 
 
 def split_by_predicate(iterable, predicate):
+    """Split an iterable into two lists the according to a predicate
+
+    Return a tuple of two lists:
+    The first has the values for which `predicate` returned True
+    The second has the values for which `predicate` returned False
+
+    :param iterable: An Iterable[T]
+    :param predicate: A function from T to bool
+    """
     true, false = [], []
     for item in iterable:
         if predicate(item):
@@ -31,22 +47,34 @@ def split_by_predicate(iterable, predicate):
 
 
 def class_as_descriptor(name):
+    """Return the JVM descriptor for the class `name`"""
     return 'L' + name + ';'
 
 
 def literal_operand(value):
+    """Create an Instruction Operand with type LITERAL and value `value`"""
     return Operand(OperandTypes.LITERAL, value)
 
 
 def constant_operand(const):
+    """Create an Instruction Operand with type CONSTANT_INDEX and value `const.index`"""
     return Operand(OperandTypes.CONSTANT_INDEX, const.index)
 
 
 def local_operand(index):
+    """Create an Instruction Operand with type LOCAL_INDEX and value `index`"""
     return Operand(OperandTypes.LOCAL_INDEX, index)
 
 
 def pull_pairs(flat):
+    """Return a Iterable of pairs of adjacent items in another Iterable
+
+    `flat` should be of even size:
+    `len(list(flat)) % 2 == 0`
+
+    >>> list(pull_pairs([1, 2, 3, 4]))
+    [(1, 2), (3, 4)]
+    """
     it = iter(flat)
     return zip(it, it)
 
@@ -63,9 +91,14 @@ def bool_to_num(b):
 
 
 def field_name_from_field_ref(ref):
-    # I'm not sure what I'm missing here.
-    # The behaviour differs between tests and actual class files which suggest that I'm not creating the field refs
-    # properly in the tests. But I've yet to figure out the problem.
+    """Return the class name of the name_and_type attribute of field references
+
+    The need arises from a weird difference between test behaviour and real class file behaviour.
+    This suggests that I'm not creating the reference properly in tests.
+    But I'm not sure exactly how.
+
+    Maintainers are welcome to solve this issue.
+    """
     name = ref.name_and_type.name.value
     try:
         return name.value
