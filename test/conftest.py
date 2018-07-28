0dae051ca4171a6f2303d6be005ec12e23b18fe3
ronyhe <ronyhe@gmail.com>
2018-07-28 17:57:43 +0300

    Changed std_loader to cache the loader instance

--- test/conftest.py
+++ test/conftest.py
@@ -18,9 +18,16 @@ def pytest_addoption(parser):
     )
 
 
+_STD_LOADER = None
+
+
 @pytest.fixture
 def std_loader(request):
-    path = request.config.getoption('--std_lib')
-    if not path:
-        raise Exception('Please supply --std_lib for proper testing ')
-    return TraditionalLoader(path)
+    global _STD_LOADER
+    if _STD_LOADER is None:
+        path = request.config.getoption('--std_lib')
+        if not path:
+            raise Exception('Please supply --std_lib for proper testing ')
+        _STD_LOADER = TraditionalLoader(path)
+
+    return _STD_LOADER
